53fb2000e6af75cdbcb2da7dcf368eb4f4390566
postjp <76644940+postjp@users.noreply.github.com>
2021-04-21 16:37:03 +0800

    Update imgx.py

--- imgx.py
+++ imgx.py
@@ -8,37 +8,48 @@ s = requests.Session()
 s.mount('http://', HTTPAdapter(max_retries=3))
 s.mount('https://', HTTPAdapter(max_retries=3))
 
-def urlget(urls):
+def urlget(urls,iofile,numone):
     try:
+        
         filenameX=[]
-        for url in urls:
-          response = s.get(url, verify=False, timeout=60)
+        if numone !=0:
+                
+            SUFFIxx=str(imghdr.what(iofile)[0])
+            iofile=iofile.replace("_2333333333333_",SUFFIxx)
+            filenameX.extend(splitimg(iofile,500,1000))
+                
+        if  numone ==0:
+            
+            for url in urls:
+                response = s.get(url, verify=False, timeout=60)
+                t = time.time()
+                filename=str(round(t * 1000000))+'_2333333333333_'
+                if response.status_code == 200:
+                    if 'Transfer-Encoding' in response.headers:
+                        with open(filename, 'wb') as f:
+                            for chunk in response.iter_content(1024):
+                                f.write(chunk)
+                            f.close()
+                            SUFFIxx=str(imghdr.what(filename)[0])
+                            filename=filename.replace("_2333333333333_",SUFFIxx)
+                            filenameX.extend(splitimg(filename,500,1000))
+                    elif 'Content-Length' in response.headers:
+                        if len(response.content) == int(response.headers['Content-Length']):
+                            with open(filename, 'wb') as f:
+                                for chunk in response.iter_content(1024):
+                                    f.write(chunk)
+                                f.close()
+                                SUFFIxx=str(imghdr.what(filename)[0])
+                                filename=filename.replace("_2333333333333_",SUFFIxx)
+                                filenameX.extend(splitimg(filename,500,1000))
 
-          t = time.time()
-
-          filename=str(round(t * 1000000))+'_2333333333333_'
-          if response.status_code == 200:
-            if 'Transfer-Encoding' in response.headers:
-                with open(filename, 'wb') as f:
-                    for chunk in response.iter_content(1024):
-                        f.write(chunk)
-                    f.close()
-                    SUFFIxx=str(imghdr.what(filename)[0])
-                    filename=filename.replace("_2333333333333_",SUFFIxx)
-                    filenameX.extend(splitimg(filename,500,1000))
-
-            elif 'Content-Length' in response.headers:
-                if len(response.content) == int(response.headers['Content-Length']):
-                    with open(filename, 'wb') as f:
-                        for chunk in response.iter_content(1024):
-                            f.write(chunk)
-                        f.close()
-                        SUFFIxx=str(imghdr.what(filename)[0])
-                        filename=filename.replace("_2333333333333_",SUFFIxx)
-                        filenameX.extend(splitimg(filename,500,1000))
-      return filenameX
+                            
+        
+        return filenameX
     except Exception as e:
+        
         try:
+            
             print('urlget:{0}'.format(traceback.format_exc()))
             return
         finally:
@@ -79,6 +90,7 @@ def splitimg(xxxx,heighss,quality,output=None):
         count=1
 
         while begin_y<H:
+            
 
             end_y=min(end_y,H)
 
@@ -93,6 +105,7 @@ def splitimg(xxxx,heighss,quality,output=None):
             begin_x,begin_y,end_x,end_y=0,end_y+1,W,end_y+1+h
         return outputAll
     except Exception as e:
+        
         try:
             print('urlget:{0}'.format(traceback.format_exc()))
             return
