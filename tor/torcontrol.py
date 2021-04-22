d2892b0f8a43082fc81bf1eea1a3ddfde08aa878
postjp <76644940+postjp@users.noreply.github.com>
2021-04-22 16:10:53 +0800

    Create torcontrol.py

---
+++ tor/torcontrol.py
@@ -0,0 +1,32 @@
+import requests,time
+from stem import Signal
+from stem.control import Controller
+  
+def switch_proxy():
+  	with Controller.from_port(port=19151) as controller:
+      	controller.authenticate(password='WWb79c214a3368ab6dcd03')
+        controller.signal(Signal.NEWNYM)
+  
+
+  
+    
+def getIP(proxy=None):
+    headers = {'Connection':'close'}
+    proxies = {}
+    if proxy != None:
+        proxies["http"] = proxy
+        proxies["https"] = proxy
+    response = requests.get("http://ip-api.com/json", headers=headers,proxies=proxies)
+    data=json.loads(response.content.decode('utf-8'))
+    if data["status"] == "success":
+        print(data["query"], data["city"])
+        
+        
+def main():
+    getIP("socks5h://127.0.0.1:19151")
+    time.sleep(7)
+    switchIP()
+    getIP("socks5h://127.0.0.1:19151")
+    
+if __name__ == '__main__':
+    main()
